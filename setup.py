5f2d585a220689fc91461c6d3680d3307afb658d
Kamila Souckova <kamila@ksp.sk>
2018-03-12 01:24:55 +0100

    packaging: semver; README as long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(name='csvtsdb',
-    version='0.1',
+    version='0.1.1',
     description='CSV-backed timeseries database usable standalone or as a Twisted resource',
+    long_description=readme,
     url='http://github.com/anotherkamila/csvtsdb',
     author='Kamila Součková',
     author_email='kamisouckova@gmail.com',
     license='MIT',
     packages=['csvtsdb'],
+    install_requires=[
+        'twisted',
+        'klein',
+    ]
 )
