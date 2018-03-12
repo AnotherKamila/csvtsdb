3524d18c7101a8479a58a63b49b5553110cb2e0e
Kamila Souckova <kamila@ksp.sk>
2018-03-12 16:50:10 +0100

    return empty response if file not found

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
-with open('README.md', 'r', 'utf-8') as f:
-    readme = f.read()
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print('warning: pypandoc module not found, could not convert Markdown to RST')
+    read_md = lambda f: open(f, 'r').read()
 
 setup(name='csvtsdb',
-    version='0.1.1',
+    version='0.1.5',
     description='CSV-backed timeseries database usable standalone or as a Twisted resource',
-    long_description=readme,
+    long_description=read_md('README.md'),
     url='http://github.com/anotherkamila/csvtsdb',
     author='Kamila Součková',
     author_email='kamisouckova@gmail.com',
